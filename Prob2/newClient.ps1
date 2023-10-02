e0db583155101ed06426e548e16c91d301878309
FrancisMaynard <francismaynard99@gmail.com>
2023-10-02 12:08:23 -0400

    Creation .ps1 file for VM Creation on  Azure Cloud Shell

--- Prob2/newClient.ps1
+++ Prob2/newClient.ps1
@@ -1,4 +1,36 @@
-CREATE WINDOWS Environment variables
+$isAdmin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
+$clientName = $args
+$vmName = $clientName + "-httpd"
+$ressourceGroupName = $clientName + "_RessGroup"
 
-$token = NewTech_Access_Token;
-$ENV[NewTech_Access_Token]
+if ($isAdmin) {
+    az group create `
+        --name $ressourceGroupName `
+        --location canadacentral 
+
+    az vm create `
+        --ressource-group $ressourceGroupName `
+        --name $vmName `
+        --image Debian `
+        --size Standard_B1s `
+        --admin-user maxime `
+        --ssh-key-values ~/.ssh/id_rsa.pub `
+        --public-ip-address-dns-name "'$vmName' + '-dns'" `
+        --location canadacentral
+
+    az network nsg rule create `
+        --resource-group $ressourceGroupName `
+        --nsg-name $vmName + "NSG" `
+        --name Allow_HTTP `
+        --priority 100 `
+        --protocol Tcp `
+        --direction Inbound `
+        --source-address-prefix '*' `
+        --source-port-range '*' `
+        --destination-address-prefix '*' `
+        --destination-port-range 80 `
+        --access Allow
+}
+else {
+    Write-Host "Vous devez être administrateur pour pouvoir exécuter le script"
+}
