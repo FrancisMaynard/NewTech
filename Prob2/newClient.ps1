fff1111100efd76cf61a90a3df7c1b96e7d7f1be
FrancisMaynard <francismaynard99@gmail.com>
2023-10-02 12:29:26 -0400

    updated .ps1 file

--- Prob2/newClient.ps1
+++ Prob2/newClient.ps1
@@ -1,36 +1,33 @@
-$isAdmin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
 $clientName = $args
-$vmName = $clientName + "-httpd"
-$ressourceGroupName = $clientName + "_RessGroup"
+$vmName = "$clientName-httpd"
+$ressourceGroupName = "$clientName-RessGroup"
 
-if ($isAdmin) {
-    az group create `
-        --name $ressourceGroupName `
-        --location canadacentral 
+az group create `
+    --name $ressourceGroupName `
+    --location canadacentral 
 
-    az vm create `
-        --ressource-group $ressourceGroupName `
-        --name $vmName `
-        --image Debian `
-        --size Standard_B1s `
-        --admin-user maxime `
-        --ssh-key-values ~/.ssh/id_rsa.pub `
-        --public-ip-address-dns-name "'$vmName' + '-dns'" `
-        --location canadacentral
+az vm create `
+    --resource-group $ressourceGroupName `
+    --name $vmName `
+    --image Debian `
+    --size Standard_B1s `
+    --admin-user maxime `
+    --ssh-key-values ~/.ssh/id_rsa.pub `
+    --public-ip-address-dns-name $vmName"-dns" `
+    --location canadacentral
 
-    az network nsg rule create `
-        --resource-group $ressourceGroupName `
-        --nsg-name $vmName + "NSG" `
-        --name Allow_HTTP `
-        --priority 100 `
-        --protocol Tcp `
-        --direction Inbound `
-        --source-address-prefix '*' `
-        --source-port-range '*' `
-        --destination-address-prefix '*' `
-        --destination-port-range 80 `
-        --access Allow
-}
-else {
-    Write-Host "Vous devez être administrateur pour pouvoir exécuter le script"
-}
+az network nsg rule create `
+    --resource-group $ressourceGroupName `
+    --nsg-name $vmName"NSG" `
+    --name Allow_HTTP `
+    --priority 100 `
+    --protocol Tcp `
+    --direction Inbound `
+    --source-address-prefix '*' `
+    --source-port-range '*' `
+    --destination-address-prefix '*' `
+    --destination-port-range 80 `
+    --access Allow
+
+
+Write-Host "VM créé"
